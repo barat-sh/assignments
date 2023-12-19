071e99ddd79897edffc7524cfa6e8ab3ded2512a
Barath Elangovan <barath_elangovan@outlook.com>
2023-12-19 22:43:55 +0530

    jwt

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -4,46 +4,51 @@ const jwtPassword = 'secret';
 
 /**
  * Generates a JWT for a given username and password.
- *
- * @param {string} username - The username to be included in the JWT payload.
- *                            Must be a valid email address.
- * @param {string} password - The password to be included in the JWT payload.
- *                            Should meet the defined length requirement (e.g., 6 characters).
- * @returns {string|null} A JWT string if the username and password are valid.
+ // *
+ // * @param {string} username - The username to be included in the JWT payload.
+ // *                            Must be a valid email address.
+ // * @param {string} password - The password to be included in the JWT payload.
+ // *                            Should meet the defined length requirement (e.g., 6 characters).
+ // * @returns {string|null} A JWT string if the username and password are valid.
  *                        Returns null if the username is not a valid email or
  *                        the password does not meet the length requirement.
  */
 function signJwt(username, password) {
-    // Your code here
+  return jwt.sign({username:username, password:password}, jwtPassword)
 }
 
+let jwt_token = signJwt("123@gmail.com", "itunes");
+console.log((jwt_token))
 /**
  * Verifies a JWT using a secret key.
- *
- * @param {string} token - The JWT string to verify.
- * @returns {boolean} Returns true if the token is valid and verified using the secret key.
- *                    Returns false if the token is invalid, expired, or not verified
+ // *
+ // * @param {string} token - The JWT string to verify.
+ // * @returns {boolean} Returns true if the token is valid and verified using the secret key.
+ // *                    Returns false if the token is invalid, expired, or not verified
  *                    using the secret key.
  */
-function verifyJwt(token) {
-    // Your code here
-}
+const verifyJwt = async (jwt_token) => {
+  return await jwt.verify(jwt_token, jwtPassword);
 
+}
+const verificationStatus = verifyJwt(jwt_token);
+console.log(verificationStatus)
 /**
  * Decodes a JWT to reveal its payload without verifying its authenticity.
- *
- * @param {string} token - The JWT string to decode.
- * @returns {object|false} The decoded payload of the JWT if the token is a valid JWT format.
- *                         Returns false if the token is not a valid JWT format.
+ // *
+ // * @param {string} token - The JWT string to decode.
+ // * @returns {object|false} The decoded payload of the JWT if the token is a valid JWT format.
+ // *                         Returns false if the token is not a valid JWT format.
  */
 function decodeJwt(token) {
-    // Your code here
+  // Your code here
 }
+
 
 
 module.exports = {
   signJwt,
-  verifyJwt,
+  // verifyJwt,
   decodeJwt,
   jwtPassword,
 };
